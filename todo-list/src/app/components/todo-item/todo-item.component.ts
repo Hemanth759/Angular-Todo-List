4334f2988676f10074d3f3e9245526916c59344c
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-07-11 10:50:50 +0530

    Displayed todos title in the webpage

--- todo-list/src/app/components/todo-item/todo-item.component.ts
+++ todo-list/src/app/components/todo-item/todo-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { Todo } from '../../models/todo';
 
@@ -9,7 +9,7 @@ import { Todo } from '../../models/todo';
 })
 export class TodoItemComponent implements OnInit {
 
-  todo: Todo;
+  @Input() todo: Todo;
 
   constructor() { 
     
